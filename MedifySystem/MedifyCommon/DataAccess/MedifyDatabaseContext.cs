40ee3bbf91cc49fab6fe7adfede295c8fe0604ed
Freddie Mason <fmason@redev.co.uk>
2024-05-18 16:18:35 +0100

    created dbcontext

--- MedifySystem/MedifyCommon/DataAccess/MedifyDatabaseContext.cs
+++ MedifySystem/MedifyCommon/DataAccess/MedifyDatabaseContext.cs
@@ -1,7 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MedifySystem.MedifyCommon.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MedifySystem.MedifyCommon.DataAccess;
-internal class MedifyDatabaseContext : DbContext
+public class MedifyDatabaseContext : DbContext
 {
+    public DbSet<User> Users { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<PatientAdmittance> PatientAdmittances { get; set; }
+    public DbSet<PatientRecord> PatientRecords { get; set; }
+    public DbSet<PatientRecordDataEntry> PatientRecordDataEntries { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().ToTable("Users");
+        modelBuilder.Entity<Patient>().ToTable("Patients");
+        modelBuilder.Entity<PatientAdmittance>().ToTable("PatientAdmittances");
+        modelBuilder.Entity<PatientRecord>().ToTable("PatientRecords");
+        modelBuilder.Entity<PatientRecordDataEntry>().ToTable("PatientRecordDataEntries");
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
