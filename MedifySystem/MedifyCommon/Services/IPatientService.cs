735029694ea0163879ed5ccef33d370bd07c423b
fM4s0n <115559911+fM4s0n@users.noreply.github.com>
2024-05-25 17:14:29 +0100

    created PatientService

---
+++ MedifySystem/MedifyCommon/Services/IPatientService.cs
@@ -0,0 +1,39 @@
+﻿using MedifySystem.MedifyCommon.Models;
+
+namespace MedifySystem.MedifyCommon.Services;
+
+/// <summary>
+/// 
+/// </summary>
+public interface IPatientService
+{     
+    /// <summary>
+    /// Get all patients
+    /// </summary>
+    /// <returns>
+    /// List of all patients
+    /// </returns>
+    List<Patient>? GetAllPatients();
+
+    /// <summary>
+    /// Get a patient by their ID
+    /// </summary>
+    /// <param name="id">The ID of the patient</param>
+    /// <returns>
+    /// The patient with the given ID
+    /// </returns>
+    Patient? GetPatientById(string id);
+
+    /// <summary>
+    /// Insert a new patient
+    /// </summary>
+    /// <param name="patient">The patient to insert</param>
+    void InsertPatient(Patient patient);
+
+    /// <summary>
+    /// Delete a patient
+    /// </summary>
+    /// <param name="patient">The patient to delete</param>
+    void DeletePatient(Patient patient);
+}
+}
---
+++ MedifySystem/MedifyCommon/Services/Implementations/Patientservice.cs
@@ -0,0 +1,33 @@
+﻿using MedifySystem.MedifyCommon.Models;
+
+namespace MedifySystem.MedifyCommon.Services.Implementations;
+
+//<inheritdoc/>
+public class PatientService : IPatientService
+{
+    private readonly IDBService? _dbService = Program.ServiceProvider!.GetService(typeof(IDBService)) as IDBService;
+
+    //<inheritdoc/>
+    public void DeletePatient(Patient patient)
+    {
+        _dbService!.DeleteEntity(patient);
+    }
+
+    //<inheritdoc/>
+    public List<Patient>? GetAllPatients()
+    {
+        return _dbService!.GetEntitiesByType<Patient>()!;
+    }
+
+    //<inheritdoc/>
+    public Patient? GetPatientById(string id)
+    {
+        return _dbService?.GetEntity<Patient>(id);
+    }
+
+    //<inheritdoc/>
+    public void InsertPatient(Patient patient)
+    {
+        _dbService?.InsertEntity(patient);
+    }
+}
