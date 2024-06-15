6e71c33357764a0c303d7308ea9a256db90ce069
fM4s0n <115559911+fM4s0n@users.noreply.github.com>
2024-06-15 14:45:55 +0100

    hospital official home page work

--- MedifySystem/MedifyDesktop/Controls/CtrFlpHomeItem.cs
+++ MedifySystem/MedifyDesktop/Controls/CtrFlpHomeItem.cs
@@ -1,11 +1,14 @@
-﻿namespace MedifySystem.MedifyDesktop.Controls
+﻿using MedifySystem.MedifyCommon.Dictionaries;
+using MedifySystem.MedifyCommon.Enums;
+
+namespace MedifySystem.MedifyDesktop.Controls
 {
     /// <summary>
     /// Item for flpHome on the home screen for headline information
     /// </summary>
     public partial class CtrFlpHomeItem : UserControl
     {
-        public CtrFlpHomeItem(string titleText, string data, string subData = "", )
+        public CtrFlpHomeItem(string titleText, string data, string subData = "", FlpHomeItemBackColour colour)
         {
             InitializeComponent();
 
@@ -15,7 +18,7 @@
             lblControlTitle.Text = titleText;
             lblControlHeadlineData.Text = data;
             lblControlSubData.Text = subData;
+            BackColor = ColorTranslator.FromHtml(FlpHomeItemBackColourDictionary.GetColour(colour));
         }
-
     }
 }
--- MedifySystem/MedifyDesktop/Controls/CtrHospitalOfficialHome.cs
+++ MedifySystem/MedifyDesktop/Controls/CtrHospitalOfficialHome.cs
@@ -1,12 +1,41 @@
-﻿namespace MedifySystem.MedifyDesktop.Controls;
+﻿using MedifySystem.MedifyCommon.Models;
+using MedifySystem.MedifyCommon.Services;
+
+namespace MedifySystem.MedifyDesktop.Controls;
 
 /// <summary>
 /// UserControl for the Hospital Official Home
 /// </summary>
 public partial class CtrHospitalOfficialHome : UserControl
 {
+    private readonly IUserService? _userService = Program.ServiceProvider!.GetService(typeof(IUserService)) as IUserService;
+
+    private readonly User? _user;
+
     public CtrHospitalOfficialHome()
     {
         InitializeComponent();
+
+        if (DesignMode)
+            return;
+
+        _user = _userService!.GetCurrentUser();
+
+        if (_user is null)
+            _userService!.LogOutUser();
+
+        Init();
+    }
+
+    private void Init()
+    {
+        SetAppointmentHomeItem();
+    }
+
+    private void SetAppointmentHomeItem()
+    {
+        // appointments today
+        int appointmentsToday = 0;
+        CtrFlpHomeItem appointmentHomeItem = new("Appointments Today",);
     }
 }
