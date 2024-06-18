70c6be15d811c92c752c06076fbc849e3f090b4d
Freddie Mason <fmason@redev.co.uk>
2024-06-18 18:17:34 +0100

    added seed random pateints

--- MedifySystem/MedifyCommon/DataAccess/DataSeeder.cs
+++ MedifySystem/MedifyCommon/DataAccess/DataSeeder.cs
@@ -2,6 +2,7 @@
 using MedifySystem.MedifyCommon.Helpers;
 using MedifySystem.MedifyCommon.Models;
 using MedifySystem.MedifyCommon.Services;
+using System;
 
 namespace MedifySystem.MedifyCommon.DataAccess;
 
@@ -13,6 +14,8 @@ public class DataSeeder
     private readonly IUserService? _userService = Program.ServiceProvider!.GetService(typeof(IUserService)) as IUserService;
     private readonly IPatientService? _patientService = Program.ServiceProvider!.GetService(typeof(IPatientService)) as IPatientService;
     private readonly IPatientAdmittanceService? _patientAdmittanceService = Program.ServiceProvider!.GetService(typeof(IPatientAdmittanceService)) as IPatientAdmittanceService;
+
+    private List<string> _usedNhsNumbers = [];
     
     /// <summary>
     /// 
@@ -77,7 +80,10 @@ public class DataSeeder
                 SeedUnadmittedPatient();
 
             if (allPatients.Any(p => p.NHSNumber == "987654321") == false)
-                SeedAdmittedPatient();
+                SeedAdmittedPatient1();
+
+            if (allPatients.Count == 2)
+                SeedRandomPatients();
         }        
     }
 
@@ -93,7 +99,7 @@ public class DataSeeder
         _patientService!.InsertPatient(patient);
     }
 
-    private void SeedAdmittedPatient()
+    private void SeedAdmittedPatient1()
     {
         Patient patient = new("Jane",
             "Doe",
@@ -104,11 +110,96 @@ public class DataSeeder
 
         _patientService!.InsertPatient(patient);
 
-        Patient createdPateint = _patientService.GetPatientByNHSNumber("987654321")!;
-
         User doctor = _userService!.GetUserByEmail("doctor@test.com")!;
 
-        PatientAdmittance admittance = new(createdPateint.Id, DateTime.Now, "Surgery", doctor.Id);
+        PatientAdmittance admittance = new(patient.Id, DateTime.Now, "Surgery", doctor.Id);
         _patientAdmittanceService!.InsertPatientAdmittance(admittance);
+    }
+
+    private void SeedRandomPatients()
+    {
+        Random random = new();
+        List<string> usedNhsNumbers = [];
+
+        for (int i = 0; i < 20; i++)
+        {
+            string firstName = $"Patient{i}";
+            string lastName = $"Surname{i}";
+            string nhsNumber = GenerateRandomNhsNumber();
+            Gender gender = GenerateRandomGender();
+            string gpName = $"Dr GP{i}";
+            DateTime dateOfBirth = GenerateRandomDateOfBirth();
+            bool admitted = random.Next(0, 2) == 1;
+
+            Patient patient = new(firstName, lastName, nhsNumber, gender, gpName, dateOfBirth);
+            _patientService!.InsertPatient(patient);
+
+            if (admitted)
+            {
+                User doctor = _userService!.GetUserByEmail("doctor@test.com")!;
+
+                PatientAdmittance admittance = new(patient.Id, DateTime.Now, "Surgery", doctor.Id);
+                _patientAdmittanceService!.InsertPatientAdmittance(admittance);
+            }
+        }
+    }
+
+    private string GenerateRandomNhsNumber()
+    {
+        Random random = new();
+        string nhsNumber = random.Next(100000000, 999999999).ToString();
+
+        int maxAttempts = 1000000;
+        int attempts = 0; 
+
+        while (_usedNhsNumbers.Contains(nhsNumber) && attempts < maxAttempts)
+        {
+            nhsNumber = random.Next(100000000, 999999999).ToString();
+            attempts++;
+        }
+
+        if (attempts >= maxAttempts)        
+            throw new Exception("Unable to generate a unique NHS number.");        
+
+        _usedNhsNumbers.Add(nhsNumber);
+
+        return nhsNumber;
+    }
+
+    private Gender GenerateRandomGender()
+    {
+        Random random = new();
+        int genderRand = random.Next(0, 4);
+
+        Gender genderValue = Gender.None;
+
+        switch (genderRand)
+        {
+            case 0:
+                genderValue = Gender.None;
+                break;
+            case 1:
+                genderValue = Gender.Male;
+                break;
+            case 2:
+                genderValue = Gender.Female;
+                break;
+            case 3:
+                genderValue = Gender.NonBinary;
+                break;
+        }
+
+        return genderValue;
+    }
+
+    private DateTime GenerateRandomDateOfBirth()
+    {
+        Random random = new();
+        DateTime startDate = new(1900, 1, 1);
+        DateTime endDate = DateTime.Now;
+
+        int dateRange = (endDate - startDate).Days;
+
+        return startDate.AddDays(random.Next(dateRange));
     }
 }
